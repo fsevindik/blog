b21acbb302f6adb67ba2c890d81d9ec6355673c3
muratcan23 <muratal0606@gmail.com>
2024-07-24 04:02:13 +0300

    make header responsive

--- client/src/componenets/Header/Header.tsx
+++ client/src/componenets/Header/Header.tsx
@@ -49,8 +49,8 @@ const Header: React.FC<HeaderProps> = ({ user }) => {
   };
 
   return (
-    <header className="bg-gray-900 text-white p-4 shadow-lg ">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
+    <header className="bg-gray-900 text-white p-4 shadow-lg">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between">
         <div className="flex items-center mb-4 sm:mb-0">
           <DoctorIcon className="w-8 h-8 text-yellow-300 mr-2" />
           <p className="text-lg font-semibold text-yellow-300">
--- client/src/componenets/Navbar/Navbar.tsx
+++ client/src/componenets/Navbar/Navbar.tsx
@@ -1,90 +1,60 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import HamburgerIcon from "../../icons/HamburgerIcon";
 import HeartIcon from "../../icons/HeartIcon";
 import HomeIcon from "../../icons/HomeIcon";
 import MessageIcon from "../../icons/MessageIcon";
+import UserIcon from "../../icons/UserIcon";
 import UserDropdown from "../Header/UserDropDown";
 
 const Navbar: React.FC<{ user: any }> = ({ user }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
-    <nav className="bg-gray-900 text-white p-4 shadow-md">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
+    <nav className="flex items-center space-x-4">
+      <NavItem to="/" icon={<HomeIcon className="w-6 h-6" />} text="Home" />
+      <NavItem
+        to="/films/trends"
+        icon={<HeartIcon className="w-6 h-6" />}
+        text="Trends"
+      />
+      <NavItem
+        to="/"
+        icon={<MessageIcon className="w-6 h-6" />}
+        text="Messages"
+      />
+      <div className="relative">
         <button
-          className="text-yellow-500 hover:text-white lg:hidden"
-          onClick={toggleMenu}
+          onClick={toggleDropdown}
+          className="text-yellow-500 hover:text-white flex items-center space-x-2"
         >
-          <HamburgerIcon className="w-6 h-6" />
+          <UserIcon className="w-6 h-6" />
+          <span className="hidden sm:inline">
+            {user ? user.name : "Profile"}
+          </span>
         </button>
-        <div
-          className={`flex flex-col lg:flex-row lg:space-x-6 ${
-            menuOpen ? "block" : "hidden"
-          } lg:flex lg:items-center`}
-        >
-          <div className="flex items-center space-x-2 py-2 lg:py-0 rounded-md hover:bg-yellow-600">
-            <Link
-              to="/"
-              className="text-yellow-500 hover:text-white flex items-center p-1"
-            >
-              <HomeIcon className="w-5 h-5" />
-              <span className="hidden sm:inline text-sm md:text-base lg:text-md ml-1">
-                Home
-              </span>
-            </Link>
+        {dropdownOpen && (
+          <div className="absolute right-0 top-full mt-2 bg-gray-800 text-white rounded-md shadow-lg z-20">
+            <UserDropdown user={user} />
           </div>
-          <div className="flex items-center space-x-2 py-2 lg:py-0 rounded-md hover:bg-yellow-600">
-            <Link
-              to="/films/trends"
-              className="text-yellow-500 hover:text-white flex items-center p-1"
-            >
-              <HeartIcon className="w-5 h-5" />
-              <span className="hidden sm:inline text-sm md:text-base lg:text-md ml-1">
-                Trends
-              </span>
-            </Link>
-          </div>
-          <div className="flex items-center space-x-2 py-2 lg:py-0 rounded-md hover:bg-yellow-600">
-            <Link
-              to="/"
-              className="text-yellow-500 hover:text-white flex items-center p-1"
-            >
-              <MessageIcon className="w-5 h-5" />
-              <span className="hidden sm:inline text-sm md:text-base lg:text-md ml-1">
-                Messages
-              </span>
-            </Link>
-          </div>
-          <div className="relative flex items-center space-x-2 py-2 lg:py-0 rounded-md hover:bg-yellow-600">
-            <div
-              className="text-yellow-500 hover:text-white flex items-center cursor-pointer p-1"
-              onClick={toggleDropdown}
-            >
-              <HamburgerIcon className="w-5 h-5 ml-1" />
-              <span className="hidden lg:inline text-sm md:text-base lg:text-md ml-2">
-                User
-              </span>
-            </div>
-            {dropdownOpen && (
-              <div className="absolute right-0 mt-2 bg-gray-800 text-white rounded-md shadow-lg z-20">
-                <UserDropdown user={user} />
-              </div>
-            )}
-          </div>
-        </div>
+        )}
       </div>
     </nav>
   );
 };
+
+const NavItem: React.FC<{
+  to: string;
+  icon: React.ReactNode;
+  text: string;
+}> = ({ to, icon, text }) => (
+  <Link to={to} className="text-yellow-500 hover:text-white flex items-center">
+    {icon}
+    <span className="hidden sm:inline ml-1">{text}</span>
+  </Link>
+);
 
 export default Navbar;

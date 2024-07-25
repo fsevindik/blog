f8eec01afdae720f9aa34e72bc4637206c0971ba
muratcan23 <muratal0606@gmail.com>
2024-07-25 17:47:33 +0300

    improve ShowFilm component style

--- client/src/pages/ShowFilm/ShowFilm.tsx
+++ client/src/pages/ShowFilm/ShowFilm.tsx
@@ -51,36 +51,57 @@ const ShowFilm: React.FC = () => {
       <BackButton />
       <h1 className="text-xl my-2 text-white font-mono">Film Info</h1>
 
-      <div className="max-w-4xl mx-auto p-4 bg-yellow-500 rounded-xl shadow-lg">
-        <img
-          src={film.bannerImageUrlB}
-          alt={film.title}
-          className="w-full h-64 object-cover rounded-lg mb-4"
-        />
-        <h1 className="text-3xl font-bold mb-2 text-gray-900">{film.title}</h1>
-        <p className="text-xl mb-2 text-gray-800">
-          Directed by {film.director}
-        </p>
-        <p className="text-lg mb-4 text-gray-700">
-          Released in {film.releaseYear}
-        </p>
-        <RatingComponent filmId={film.filmId} ratings={film.ratings} />
-        <h2 className="text-2xl font-semibold mt-4 mb-2 text-gray-900">
-          Overview
-        </h2>
-        <p className="text-gray-800">{film.filmOverview}</p>
-        <h2 className="text-2xl font-semibold mt-4 mb-2 text-gray-900">Cast</h2>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-          {film.actors.map((actor, index) => (
-            <div key={index} className="text-center">
-              <img
-                src={actor.imageUrl}
-                alt={actor.name}
-                className="w-24 h-24 object-cover rounded-full mx-auto mb-2"
-              />
-              <p className="text-gray-900">{actor.name}</p>
+      <div className="max-w-4xl mx-auto p-4 bg-yellow-500 rounded-xl shadow-lg space-y-4">
+        <div className="flex flex-col md:flex-row p-1">
+          <div className="w-full md:w-1/2 pr-0 md:pr-2 mb-4 md:mb-0 flex justify-center md:justify-start p-1 ">
+            <img
+              src={film.posterImageUrlA}
+              alt={film.title}
+              className="rounded-lg shadow-lg w-full h-auto object-cover border-2 border-gray-700 mb-4 md:mb-0"
+            />
+          </div>
+          <div className="hidden md:block w-full md:w-1/2 pl-0 md:pl-2 p-1">
+            <img
+              src={film.bannerImageUrlB}
+              alt={film.title}
+              className="rounded-lg shadow-lg w-full h-full object-cover border-2 border-gray-700"
+            />
+          </div>
+        </div>
+        <div className="space-y-4">
+          <div>
+            <h1 className="text-3xl font-bold mb-2 text-gray-900">
+              {film.title}
+            </h1>
+            <p className="text-xl mb-2 text-gray-800">
+              Directed by {film.director}
+            </p>
+            <p className="text-lg mb-4 text-gray-700">
+              Released in {film.releaseYear}
+            </p>
+            <RatingComponent filmId={film.filmId} ratings={film.ratings} />
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold mb-2 text-gray-900">
+              Overview
+            </h2>
+            <p className="text-gray-800">{film.filmOverview}</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-semibold mb-2 text-gray-900">Cast</h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+              {film.actors.map((actor, index) => (
+                <div key={index} className="text-center">
+                  <img
+                    src={actor.imageUrl}
+                    alt={actor.name}
+                    className="w-24 h-24 object-cover rounded-full mx-auto mb-2 border-2 border-gray-700"
+                  />
+                  <p className="text-gray-900">{actor.name}</p>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </div>
 

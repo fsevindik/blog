d1a5d66b4403234394b0c25e0609884fa111a746
muratcan23 <muratal0606@gmail.com>
2024-08-01 14:19:50 +0300

    improve style

--- client/src/pages/ShowFilm/ShowFilmInfo.tsx
+++ client/src/pages/ShowFilm/ShowFilmInfo.tsx
@@ -94,13 +94,16 @@ const ShowFilmInfo: React.FC<FilmInfoProps> = ({
           </h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
             {film.actors.map((actor, index) => (
-              <div key={index} className="text-center">
+              <div
+                key={index}
+                className=" flex flex-col items-center text-center"
+              >
                 <img
                   src={actor.imageUrl}
                   alt={actor.name}
                   className="w-20 h-28 sm:w-24 sm:h-32 object-cover p-1 rounded-md -auto mb-2 border-1 bg-slate-100 border-gray-800"
                 />
-                <p className="text-gray-900 text-sm sm:text-base">
+                <p className="text-gray-900 md:text-sm sm:text-xs font-semibold ">
                   {actor.name}
                 </p>
               </div>

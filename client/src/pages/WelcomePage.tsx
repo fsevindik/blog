7985338d9d8091a3e9d198f4618e0eff76108179
muratcan23 <muratal0606@gmail.com>
2024-08-11 22:12:23 +0300

    improve UI and UX

--- client/src/pages/WelcomePage.tsx
+++ client/src/pages/WelcomePage.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import FavoriteFilms from "./components/FavoriteFilms";
 
 const Welcome: React.FC = () => {
   const userName = localStorage.getItem("userName");
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
 
   return (
     <div className="flex flex-col md:flex-row h-full items-center justify-center min-h-screen bg-yellow-600">
-      <div className="md:w-1/2 md:ml-4 flex-shrink-0 m-5">
+      <div className="md:w-1/3 md:ml-4 flex-shrink-0 m-5">
         <FavoriteFilms />
       </div>
       <div className="bg-white text-black p-6 rounded-md shadow-md w-full max-w-md text-center md:w-1/2 md:mr-4">
@@ -16,26 +21,34 @@ const Welcome: React.FC = () => {
         </h2>
         <p>Congrats... !!! You have successfully logged in.</p>
         <p className="mb-4">Enjoy exploring movies from my own picks !</p>
-        <div className="space-y-4">
-          <div className="font-semibold font-mono">
-            <span className="underline">💬 </span>
-            You can comment on any of them.
-            <hr className="my-2" />
+        <button
+          onClick={toggleDescription}
+          className="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded mb-4"
+        >
+          {showDescription ? "Hide Description" : "Show Description"}
+        </button>
+        {showDescription && (
+          <div className="space-y-4">
+            <div className="font-semibold font-mono">
+              <span className="underline">💬 </span>
+              You can comment on any of them.
+              <hr className="my-2" />
+            </div>
+            <div className="font-semibold font-mono">
+              <span>💛 </span> Don't forget to collect your own favorites.
+              <hr className="my-2" />
+            </div>
+            <div className="font-semibold font-mono">
+              <span>👍👎 </span> Now you can rate any of them.
+              <hr className="my-2" />
+            </div>
+            <div className="font-semibold font-mono">
+              📨 Also, you can send message to admin, you have direct line to
+              him, by clicking on the blue dot🔵 on the right bottom side. ↘️
+              <hr className="my-2" />
+            </div>
           </div>
-          <div className="font-semibold font-mono">
-            <span>💛 </span> Dont forget to collet your own favorites .
-            <hr className="my-2" />
-          </div>
-          <div className="font-semibold font-mono">
-            <span>👍👎 </span> Now you can rate any of them.
-            <hr className="my-2" />
-          </div>
-          <div className="font-semibold font-mono">
-            📨 Also, you can send message to admin, you have direct line to him,
-            by clicking on the blue dot🔵 on the rigth bottom side. ↘️
-            <hr className="my-2" />
-          </div>
-        </div>
+        )}
         <button className="bg-black text-white py-2 px-4 rounded-md animate-pulse mt-4">
           <Link to="/" className="flex items-center hover:text-gray-300">
             Explore Films

f91ddbb35142fbb870351b8e1b5e588d0582cbef
Murat Can <muratal0606@gmail.com>
2024-10-17 03:38:41 +0300

    suspend wishlist for now

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import filmRoutes from "./routes/filmRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import reactionRoutes from "./routes/reactionRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import wishListRoutes from "./routes/wishlistRoutes.js"
+//import wishListRoutes from "./routes/wishlistRoutes.js"
 
 
 dotenv.config();
